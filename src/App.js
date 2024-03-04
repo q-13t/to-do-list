28d95b0c7eb9290fd0ab72a5e2a39974fda579e3
q-13t <vova29092002@gmail.com>
2024-03-04 15:18:21 +0200

    Fin touches

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+// import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import NotFound from './NotFound';
 import HomePage from './HomePage';
@@ -11,7 +11,7 @@ function App() {
             <div className="App">
                 <Routes>
                     <Route path='/' element={<HomePage />}></Route>
-                    <Route exact path='/login' element={<Login />}></Route>// TODO: Implement login page
+                    <Route exact path='/login' element={<Login />}></Route>
                     <Route exact path='/todo' element={<ToDo />}></Route>
                     <Route path='*' element={<NotFound />}></Route>
                 </Routes>
@@ -24,7 +24,7 @@ export default App;
 
 // Important RUN: npm install
 
-// To start react server do: npm start
+// To start react server do: npm run start
 // To start json server do: npx json-server --watch data-base/db.json -p 8000
 
 // NOTE: press Ctrl + C to cancel both or simply close the terminal
